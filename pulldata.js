af0b910e95b4ab1cb348b6eb9cdbce92af2c067c
mjbuchman <mbookie213@gmail.com>
2020-10-06 19:03:23 -0400

    Import node modules

---
+++ pulldata.js
@@ -0,0 +1,4 @@
+import { ... } from 'espn-fantasy-football-api'; // web
+import { ... } from 'espn-fantasy-football-api/node'; // node
+import { ... } from 'espn-fantasy-football-api/web-dev'; // web development build
+import { ... } from 'espn-fantasy-football-api/node-dev'; // node development build
