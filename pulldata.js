d4e8059ba88da40fdb179be57686fd2af8ce7537
mjbuchman <mbookie213@gmail.com>
2020-10-30 17:17:41 -0400

    removed dep

--- pulldata.js
+++
@@ -1,4 +0,0 @@
-import { ... } from 'espn-fantasy-football-api'; // web
-import { ... } from 'espn-fantasy-football-api/node'; // node
-import { ... } from 'espn-fantasy-football-api/web-dev'; // web development build
-import { ... } from 'espn-fantasy-football-api/node-dev'; // node development build
