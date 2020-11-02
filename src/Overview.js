85dbfee180a468bf5cf66822aeacc2acff0ae5bb
mjbuchman <mbookie213@gmail.com>
2020-11-02 14:24:38 -0500

    Team overview template created

--- src/Overview.js
+++ src/Overview.js
@@ -1,18 +1,76 @@
 import React, { Component } from "react";
+import "./css/overview.css"
  
 class Overview extends Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <div className="team-overview">
+            <div className="item1">
+                <h3>Owner:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item2">
+                <h3>Seasons Played:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item3">
+                <h3>Games Played:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item4">
+                <h3>Record:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item5">
+                <h3>Total Points For:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item6">
+                <h3>Total Points Against:</h3>
+                <h2>Insert Value</h2>
+            </div>
+            <div className="item7">
+                <h3>Average Points For:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item8">
+                <h3>Average Points Against:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item9">
+                <h3>Highest Score:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item10">
+                <h3>Lowest Score:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item11">
+                <h3>Biggest Win Margin:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item12">
+                <h3>Smallest Win Margin:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item13">
+                <h3>Biggest Loss Margin:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+            <div className="item14">
+                <h3>Smallest Loss Margin:</h3>
+                <h2>Insert Value</h2>
+                <p>Subtitle</p>
+            </div>
+        </div>
       </div>
     );
   }
