018ec6c3974b5246bea69e444b2c3ef9bb01087e
mjbuchman <mbookie213@gmail.com>
2020-11-02 11:24:32 -0500

    Basic SPA routing set up

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
+  import Overview from "./Overview";
+  import HeadToHead from "./HeadToHead";
+  import Standings from "./Standings";
+  import Records from "./Records";
  
 class Main extends Component {
   render() {
     return (
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Overview</a></li>
-            <li><a href="/h2h">Head to Head</a></li>
-            <li><a href="/standings">Standings</a></li>
-            <li><a href="/records">Records</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
-        </div>
+        <HashRouter>
+            <div>
+                <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li><NavLink to="/">Overview</NavLink></li>
+                    <li><NavLink to="/h2h">Head to Head</NavLink></li>
+                    <li><NavLink to="/standings">Standings</NavLink></li>
+                    <li><NavLink to="/records">Records</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Overview}/>
+                    <Route path="/h2h" component={HeadToHead}/>
+                    <Route path="/standings" component={Standings}/>
+                    <Route path="/records" component={Records}/>
+                </div>
+            </div>
+        </HashRouter>
     );
   }
 }
---
+++ src/Records.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Records extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Records here</h2>
+      </div>
+    );
+  }
+}
+ 
+export default Records;
