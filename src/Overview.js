16c8462505d11315425b2bed6e7461b9a149d11e
mjbuchman <mbookie213@gmail.com>
2020-11-03 12:35:26 -0500

    right side dashboard complete - frontend

--- src/Overview.js
+++ src/Overview.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./css/overview.css"
+import ReactSpeedometer from "react-d3-speedometer"
  
 class Overview extends Component {
   render() {
@@ -80,8 +81,130 @@ class Overview extends Component {
                     </div>    
                 </div>
                 <div className="col-sm-7">
-                    <title>Regular Season</title>
-                    <content>Regular Season</content>
+                    <h4>Regular Season Performance</h4>
+                    <div id="box">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <h5>Win Rate</h5>
+                                <ReactSpeedometer
+                                    maxValue={1.000}
+                                    width={190}
+                                    height={140}
+                                    needleHeightRatio={0.7}
+                                    maxSegmentLabels={4}
+                                    segments={1000}
+                                    value={.333}
+                                    textColor='#777'
+                                    />
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Win</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Loss</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Tie</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Regular Season Champion</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Last Place Finishes</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Best Placement</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                    <h4>Playoff Performance</h4>
+                    <div id="box">
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <h5>Win Rate</h5>
+                                <ReactSpeedometer
+                                    maxValue={1.000}
+                                    width={190}
+                                    height={140}
+                                    needleHeightRatio={0.7}
+                                    maxSegmentLabels={4}
+                                    segments={1000}
+                                    value={.333}
+                                    textColor='#777'
+                                    />
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Win</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Loss</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                            <div className="col-sm-3">
+                                <h5>Tie</h5>
+                                <hr></hr>
+                                <h1>VAL</h1>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Playoff Appearances</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Championship Appearances</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="stats-box">
+                                    <h6>Championships</h6>
+                                    <div id="box">
+                                        <h1 id="small-mar">VAL</h1>
+                                        <p id="black">Subtitle</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
                 </div>
             </div>
         </div>
