d7673d6b0a3ecff65b6c2610102ef8ba7e5b8a29
mjbuchman <mbookie213@gmail.com>
2020-11-03 13:31:32 -0500

    yearly graph for feature overview

--- src/Overview.js
+++ src/Overview.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
 import "./css/overview.css"
 import ReactSpeedometer from "react-d3-speedometer"
+import {Line} from 'react-chartjs-2';
  
+const data = {
+    labels: ['2017', '2018', '2019', '2020'],
+    datasets: [
+      {
+        label: 'Wins By Year',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [9,4,7,6],
+      }
+    ]
+  };
+
+ const options = {
+    scales: {
+        yAxes: [
+            {   
+                ticks: {
+                    min: 0,
+                    max: 14,
+                },
+            },
+        ],
+    },
+}
+  
+
+
 class Overview extends Component {
   render() {
     return (
@@ -205,6 +248,14 @@ class Overview extends Component {
                             </div>
                         </div>
                     </div>  
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h4>Yearly Performance</h4>
+                    <div id="box">
+                        <Line data={data} height={80} options={options}/>
+                    </div>
                 </div>
             </div>
         </div>
