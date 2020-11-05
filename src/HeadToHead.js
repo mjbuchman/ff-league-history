99e44edcc37ce63d2784039b8effc8ff134ebb93
mjbuchman <mbookie213@gmail.com>
2020-11-04 20:12:35 -0500

    init layout of h2h windows

--- src/HeadToHead.js
+++ src/HeadToHead.js
@@ -3,9 +3,28 @@ import React, { Component } from "react";
 class HeadToHead extends Component {
   render() {
     return (
-      <div>
-      
-      </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-8">
+                    <select id="owners">
+                        <option value="Michael Buchman">Michael Buchman</option>
+                        <option value="Joe Perry">Joe Perry</option>
+                        <option value="James Earley">James Earley</option>
+                    </select>
+                    <select id="owners">
+                        <option value="Michael Buchman">Michael Buchman</option>
+                        <option value="Joe Perry">Joe Perry</option>
+                        <option value="James Earley">James Earley</option>
+                    </select>
+                </div>
+                <div className="col-sm-4">
+                    <h4>Gamelogs</h4>
+                    <div id="box">
+                        <p>SAMPLE</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
   }
 }
