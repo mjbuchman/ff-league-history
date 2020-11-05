0d698f817d1410268d59d3c96851cb01d4a4cf7d
mjbuchman <mbookie213@gmail.com>
2020-11-04 20:07:21 -0500

    branched and reset h2h page

--- src/HeadToHead.js
+++ src/HeadToHead.js
@@ -4,16 +4,7 @@ class HeadToHead extends Component {
   render() {
     return (
       <div>
-        <h2>HeadToHead</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
+      
       </div>
     );
   }
