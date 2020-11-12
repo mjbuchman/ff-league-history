c3d637b0981ce9579d05ec00849e8fbd55adee4f
mjbuchman <mbookie213@gmail.com>
2020-11-11 22:34:21 -0500

    standings table populated (minus rank)

--- src/Standings.js
+++ src/Standings.js
@@ -9,7 +9,9 @@ let data = [
 ];
 
 let fields = [
+	{ name: 'rank', displayName: "Rank", thClassName: "standings-th", tdClassName: "standings-td" },
 	{ name: 'owner', displayName: "Owner", thClassName: "standings-th", tdClassName: "standings-td" },
+	{ name: 'gp', displayName: "Games Played", thClassName: "standings-th", tdClassName: "standings-td", inputFilterable: true, exactFilterable: true, sortable: true },
 	{ name: 'win', displayName: "Wins", thClassName: "standings-th", tdClassName: "standings-td", inputFilterable: true, exactFilterable: true, sortable: true },
 	{ name: 'loss', displayName: "Losses", thClassName: "standings-th", tdClassName: "standings-td", inputFilterable: true, exactFilterable: true, sortable: true },
 	{ name: 'tie', displayName: "Ties", thClassName: "standings-th", tdClassName: "standings-td", inputFilterable: true, exactFilterable: true, sortable: true },
@@ -38,7 +40,22 @@ class Standings extends Component {
             },
             //make sure to serialize your JSON body
             body: JSON.stringify({
-                query: 'select distinct home_team as owner from Matchups'
+                //query: `SELECT owner, SUM(pf)as pf, SUM(pa) as pa FROM (SELECt home_team as owner, SUM(home_score) as pf, SUM(away_score) as pa FROM Matchups GROUp BY Owner UNION ALL SELECt away_team as Owner, SUM(away_score) as pf, SUM(home_score) as pa FROM Matchups GROUP BY Owner)as x GROUP BY Owner Order BY pf DESC`
+                query: `select firstJoin.owner, win+loss as gp, win, loss, 0 as tie, win/(win+loss) as pct, pf, pa from
+
+                (select outerPoints.owner, win, pf, pa  from
+                (SELECT owner, SUM(pf)as pf, SUM(pa) as pa FROM (SELECT home_team as owner, SUM(home_score) as pf, SUM(away_score) as pa FROM Matchups GROUp BY Owner UNION ALL SELECt away_team as Owner, SUM(away_score) as pf, SUM(home_score) as pa FROM Matchups GROUP BY Owner) as innerPoints group by owner) outerPoints
+                inner join
+                (SELECT owner, COUNT(*) as win FROM (Select home_team as owner from Matchups WHERE home_score > away_score UNION ALL Select away_team as owner from Matchups WHERE away_score > home_score) as innerWins GROUP By owner) outerWins
+                on (outerPoints.owner = outerWins.owner)) firstJoin 
+                
+                inner join
+                
+                (SELECT owner, COUNT(*) as loss FROM (Select home_team as owner from Matchups WHERE home_score < away_score UNION ALL Select away_team as owner from Matchups WHERE away_score < home_score) as innerLosses GROUP By owner) outerLosses
+                
+                on (firstJoin.owner = outerLosses.owner) where firstJoin.owner != "Sal DiVita" AND firstJoin.owner != "Zach Way" order by win desc
+                
+                `
             })
         })
         .then((response) => response.json())
@@ -67,7 +84,7 @@ class Standings extends Component {
                         <div id="box">
                             <FilterableTable
                                 namespace="People"
-                                initialSort="rank"
+                                initialSort="pct"
                                 data={this.state.data}
                                 fields={fields}
                                 tableClassName="standings-table"
